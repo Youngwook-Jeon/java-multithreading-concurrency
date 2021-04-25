c5f177480e1e1de4991a0462cb3bab664fda2155
Young wook Jeon <ela87@naver.com>
2021-04-25 21:45:17 +0900

    first commit

---
+++ src/part1/ThreadsCreation.java
@@ -0,0 +1,24 @@
+package part1;
+
+public class ThreadsCreation {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            // Code that will run in a new thread
+            System.out.println("We are now in thread " + Thread.currentThread().getName());
+            System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+//            throw new RuntimeException("Intentional Exception");
+        });
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY); // 10
+        thread.setUncaughtExceptionHandler((t, e) -> {
+            System.out.println("A critical error happened in thread " + t.getName() +
+                    " the error is " + e.getMessage());
+        });
+        System.out.println("We are in thread: " + Thread.currentThread().getName() +
+                " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName() +
+                " after starting a new thread");
+    }
+}
